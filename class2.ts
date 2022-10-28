3c68227b7b983853973fb70741b3a49ef1d9ed4d
Steve <ycsteven.tsao@gmail.com>
2022-10-28 15:37:03 +0800

    class 訪問類型，構造器

--- class2.ts
+++ class2.ts
@@ -1,10 +1,37 @@
+// class Person {
+//   protected name1: string;
+//   sayHi() {
+//     console.log('hi');
+//   }
+// }
+
+// class Teacher extends Person {
+//   sayHi(): void {
+//     this.name1;
+//   }
+// }
+
+// constructor
+// class Person {
+//   // public name1: string;
+//   constructor(public name1: string) {
+//     // this.name1 = name;
+//   }
+// }
+
+// const person = new Person('MOMO');
+// console.log(person.name1);
+
 class Person {
-  private name1: string;
-  sayHi() {
-    console.log('hi');
+  constructor(public name1: string) {}
+}
+
+class Teacher extends Person {
+  constructor(public age: number) {
+    super('JOJO');
   }
 }
-const person = new Person();
-person.name1 = 'JOJO';
-console.log(person.name1);
-person.sayHi();
+
+const teacher1 = new Teacher(28);
+console.log(teacher1.age);
+console.log(teacher1.name1);
